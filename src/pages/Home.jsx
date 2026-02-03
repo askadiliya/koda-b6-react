9f257b2a8ddaa2445422044d0d104af03879cd08
askadiliya <askadiliyan@gmail.com>
2026-02-03 11:05:27 +0700

    feat: create section card and visit store

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,48 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const URL =
+    "https://raw.githubusercontent.com/askadiliya/koda-b6-html/refs/heads/main/data/data.json";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(URL);
+        if (!res.ok) throw new Error("Network response was not ok");
+
+        const data = await res.json();
+        setProducts(data.flashSaleProducts.slice(0, 4) || []);
+        
+      } catch (error) {
+        console.error("Gagal mengambil data produk:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-
-      <section className="grid grid-cols-6">
-        <div className="col-span-3 flex flex-col justify-center gap-6 p-5 bg-gradient-to-b from-[#777C82] to-[#0B0909] md:p-20">
+      {/* HERO SECTION */}
+      <section className="grid grid-cols-1 md:grid-cols-6">
+        <div className="md:col-span-3 flex flex-col justify-center gap-6 p-5 bg-gradient-to-b from-[#777C82] to-[#0B0909] md:p-20">
           <h1 className="text-4xl md:text-5xl font-bold text-white">
             Start Your Day with <br /> Coffee and Good <br /> Meals
           </h1>
 
           <p className="text-white">
             We provide high quality beans, good taste, and healthy meals made by
-            love just for you. Start your day with us for a bigger smile!
+            love just for you.
           </p>
 
           <a
-            href="#"
+            href="#menu"
             className="bg-[#FF8906] text-white font-semibold px-6 py-3 rounded-lg w-max"
           >
             Get Started
@@ -38,31 +64,62 @@ function Home() {
           </div>
         </div>
 
-        <div className="col-span-3">
+        <div className="md:col-span-3">
           <img
-            className="h-screen w-full object-cover"
+            className="h-full w-full object-cover"
             src="/img/Rectangle 287.png"
             alt="Hero"
           />
         </div>
       </section>
 
-      <div className="flex flex-col-reverse md:flex-row h-512 md:h-screen md:max-h-192">
-                <div className="copytext-container flex-1 flex items-center justify-center">
-                    <div className="copytext w-[80%] px-6 py-10 flex flex-col gap-4">
-                        <h2 className="copytext-headline text-5xl border-l-4 pl-4">We Provide <span className="text-[#8E6447]">Good Coffee</span> And <span className="text-[#8E6447]">Healthy Meals</span></h2>
-                        <p className="">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
-                        <ul className="list-image-[url(/src/assets/img/centang.svg)] flex flex-col gap-8">
-                            <li>High quality beans</li>
-                            <li>Healthy meals, you can request the ingredients</li>
-                            <li>Chat with our staff to get better experience for ordering</li>
-                            <li>Free member card with a minimum purchase of IDR 200.000.</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="hero-image flex-1 h-128 md:h-auto bg-[url('/src/assets/img/herro.png')] bg-cover bg-center bg-no-repeat">
-                </div>
-            </div>
+      <section className="grid grid-cols-1 md:grid-cols-6">
+        <div className="md:col-span-3 flex flex-col justify-center gap-6 p-5 md:p-20">
+            <h2 className="copytext-headline text-5xl border-l-4 pl-4 ">We Provide <span className="text-[#8E6447]">Good Coffee</span> And <span className="text-[#8E6447]">Healthy Meals</span></h2>
+            <p className="">You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
+            <ul className="list-image-[url(/src/assets/img/centang.svg)] flex flex-col gap-8">
+              <li>High quality beans</li>
+              <li>Healthy meals, you can request the ingredients</li>
+              <li>Chat with our staff to get better experience for ordering</li>
+              <li>Free member card with a minimum purchase of IDR 200.000.</li>
+            </ul>
+        </div>
+
+        <div className="md:col-span-3">
+          <img
+            className="h-full w-full object-cover"
+            src="/img/herro.png"
+            alt="Hero"
+          />
+        </div>
+      </section>
+
+      {/* MENU SECTION */}
+      <section id="menu" className="p-6">
+        <h1 className="text-2xl font-semibold mb-6 text-center">Here is People’s <span className="text-[#8E6447]">Favorite</span> </h1>
+        <p className="text-center text-gray-600 mb-8">Discover our most popular menu items that customers love!</p>
+
+        {loading ? (
+          <p className="text-gray-500">Loading products...</p>
+        ) : products.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {products.map((item) => (
+              <ProductCard key={item.id} product={item} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-red-500">Produk tidak tersedia</p>
+        )}
+      </section>
+
+      <section>
+        <h1 className="text-2xl font-semibold mb-6 text-center">Visit Our Store in the Spot on the Map Below<span className="text-[#8E6447]">Favorite</span> </h1>
+        <div>
+          <img src="/img/Huge Global.png" alt="Map" className="w-full min-h-full object-cover"/>
+        </div>
+      </section>
+
+
     </div>
   );
 }
