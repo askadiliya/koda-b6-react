205f277c02d793f30433118a5a70f3164e3d17a8
askadiliya <askadiliyan@gmail.com>
2026-02-06 14:46:36 +0700

    feat: add component input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,26 +1,18 @@
 import React from "react";
 
-function Input(props) {
-  const { type, name, label, id, value, placeholder, onChange, icon } =props
+const Input = React.forwardRef(
+  ({ type = "text", name, placeholder, ...rest }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        {...rest}
+        className="w-full p-3 mb-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#FF8906]"
+      />
+    );
+  }
+);
 
-  return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-
-      <label className="flex items-center gap-2 border border-gray-300 rounded-md px-3 py-2 mt-1">
-        <span>{icon}</span>
-        <input
-          type={type}
-          name={name}
-          id={id}
-          value={value}
-          placeholder={placeholder}
-          onChange={onChange}
-          className="flex-1 border-none outline-none"
-        />
-      </label>
-    </div>
-  );
-}
-
-export default Input
+export default Input;
