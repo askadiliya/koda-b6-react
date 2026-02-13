39da5384989336cad17d341705d340bc4ade3fab
askadiliya <askadiliyan@gmail.com>
2026-02-13 13:54:56 +0700

    feat: update router with product detail and payment pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
+// Import Pages & Components
 import Home from "./pages/Home";
 import AppLayout from "./components/AppLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProductPage from "./pages/ProductPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import Users from "./pages/Users";
+import ProductDetail from "./pages/ProductDetail";
+import PaymentDetailsPage from "./pages/PaymentDetailsPage";
+
+
 
 const router = createBrowserRouter([
   {
@@ -20,6 +26,27 @@ const router = createBrowserRouter([
           </ProtectedRoute>
         ),
       },
+      {
+        path: "/product",
+        element: <ProductPage />,
+      },
+      {
+        
+        path: "/product/:id", 
+        element: <ProductDetail />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+       {
+        path: "/productdetail",
+        element: <ProductDetailPage/>,
+      },
+      {
+        path: "/payment",
+        element: <PaymentDetailsPage/>,
+      },
     ],
   },
   {
@@ -30,10 +57,6 @@ const router = createBrowserRouter([
     path: "/register",
     element: <RegisterPage />,
   },
-  {
-    path: "/product",
-    element: <ProductPage/>
-  }
 ]);
 
 function App() {
