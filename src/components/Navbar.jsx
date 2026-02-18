d40dca08665a43193382c49cb02d1a76da6d697d
askadiliya <askadiliyan@gmail.com>
2026-02-18 17:07:39 +0700

    feat: responsive mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
-      <header className="flex h-16 w-full justify-between  bg-black/70 backdrop-blur-md border-b border-white/10 px-5  "> 
+      <header className="flex h-16 w-full justify-between  bg-black/70 backdrop-blur-md border-b border-white/10 px-5 md:px-15 "> 
         <div className="flex items-center  gap-3">
           <div>
-          <img src="/img/LogoWhite.png" alt="" />
+          <img src="/img/LogoWhite.png" alt="coffe-akh" />
           </div>
           <div>
-            <ul className="flex text-white">
+            <ul className="hidden  md:flex gap-5 text-white">
               <li>
                 <Link>Home</Link>
               </li>
@@ -25,24 +25,24 @@ function Navbar() {
           </div>
         </div>
 
-        <div className="flex items-center ">
-          <div className="hidden">
-            <AiOutlineSearch />
-            <HiShoppingCart />
+        <div className="flex items-center gap-5">
+          <div className="hidden md:flex md:gap-5">
+            <AiOutlineSearch className="text-white text-3xl"/>
+            <HiShoppingCart className="text-white text-3xl"/>
           </div>
-          <div className="hidden">
-            <ul>
-              <li>
+          <div className="hidden md:flex">
+            <ul className="flex gap-5">
+              <li className="bg-amber-400 px-5 py-3 rounded">
                 <Link>SignIn</Link>
               </li>
-              <li>
+              <li className="bg-amber-400 px-5 py-3 rounded">
                 <Link>SignUp</Link>
               </li>
               
             </ul>
           </div>
           <div>
-          <GiHamburgerMenu  className="text-white text-3xl "/>
+          <GiHamburgerMenu  className="text-white text-3xl md:hidden"/>
           </div>
         </div>
 
